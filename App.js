da1aa2aacf482501f63b5dae24a58d9fbfbb61f8
sgoyal <sgoyal@godaddy.com>
2023-09-04 18:57:58 -0700

    Lecture-3: JSX and basic component

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const child = React.createElement("h2", { id: "child" }, "I am h2 tag!!");
-const sibling = React.createElement("h2", { id: "sibling" }, "I am a sibling");
-const parent = React.createElement("div", { id: "parent" }, [child, sibling]);
-
-console.log(parent); // returns a javascript object
+const Title = () => {
+  return <h1>Title of the header component</h1>;
+};
+const HeadingComponent = () => {
+  return (
+    <div className="container">
+      <Title />
+      <h2>Content of the header</h2>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent); // convert the object to h1 tag to render it on screen.
+root.render(<HeadingComponent />);
